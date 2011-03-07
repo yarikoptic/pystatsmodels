23a9af489521c32d256b924212483a291c8836e0
Skipper Seabold <jsseabold@gmail.com>
2011-03-07 14:11:47 -0500

    Changed expressed to express in copyright notices

--- scikits/statsmodels/datasets/star98/data.py
+++ scikits/statsmodels/datasets/star98/data.py
@@ -42,7 +42,7 @@ __all__ = ['COPYRIGHT','TITLE','SOURCE','DESCRSHORT','DESCRLONG','NOTE', 'load']
 
 __docformat__ = 'restructuredtext'
 
-COPYRIGHT   = """Used with expressed permission from the original author, 
+COPYRIGHT   = """Used with express permission from the original author,
 who retains all rights."""
 TITLE       = "Star98 Educational Dataset"
 SOURCE      = """
@@ -58,7 +58,7 @@ results for 1998.  The data measured standardized testing by the California
 Department of Education that required evaluation of 2nd - 11th grade students
 by the the Stanford 9 test on a variety of subjects.  This dataset is at
 the level of the unified school district and consists of 303 cases.  The
-binary response variable represents the number of 9th graders scoring 
+binary response variable represents the number of 9th graders scoring
 over the national median value on the mathematics exam.
 
 The original source files and information are included in /star98/src/
@@ -67,7 +67,7 @@ The data used in this example is only a subset of the original source.
 """
 
 NOTE        = """
-Number of Observations: 303 (counties in California). 
+Number of Observations: 303 (counties in California).
 Number of Variables: 13 and 8 interaction terms.
 Definition of variables names:
     NABOVE - Total number of students above the national median for the math
@@ -88,10 +88,10 @@ Definition of variables names:
     PCTAF - Percentage of students taking UC/CSU prep courses
     PCTCHRT - Percentage of charter schools
     PCTYRRND - Percentage of year-round schools
-    
+
     The below variables are interaction terms of the variables defined above.
 
-    PERMINTE_AVYRSEXP 
+    PERMINTE_AVYRSEXP
     PEMINTE_AVSAL
     AVYRSEXP_AVSAL
     PERSPEN_PTRATIO
@@ -108,10 +108,10 @@ from os.path import dirname, abspath
 def load():
     """
     Load the star98 data and returns a Dataset class instance.
-   
+
     Returns
     -------
-    Load instance: 
+    Load instance:
         a class of the data with array attrbutes 'endog' and 'exog'
     """
     filepath = dirname(abspath(__file__))
@@ -121,7 +121,7 @@ def load():
             "PCTCHRT","PCTYRRND","PERMINTE_AVYRSEXP","PERMINTE_AVSAL",
             "AVYRSEXP_AVSAL","PERSPEN_PTRATIO","PERSPEN_PCTAF","PTRATIO_PCTAF",
             "PERMINTE_AVYRSEXP_AVSAL","PERSPEN_PTRATIO_PCTAF"]
-    data = recfromtxt(filepath + '/star98.csv', delimiter=",", 
+    data = recfromtxt(filepath + '/star98.csv', delimiter=",",
             names=names, skip_header=1, dtype=float)
     names = list(data.dtype.names)
     # endog = (successes, failures)
